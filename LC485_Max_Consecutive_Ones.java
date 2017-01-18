c7e21a6d5e15caae8cd69491010e452e8213cd28
Rick Fu Zheng <rfzheng@eutility.com.au>
2017-01-18 16:19:07 +1100

    max value

---
+++ LC485_Max_Consecutive_Ones.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int result = 0;
+        int count = 0;
+        for(int i = 0; i< nums.length; i++){
+            if(nums[i] == 1){
+                count ++;
+            }else{
+                if(count > result){
+                    result = count;
+                    
+                }
+                count = 0;
+            }
+        }
+        if(count > result){
+            result = count;
+        }
+        return result;
+    }
+}
